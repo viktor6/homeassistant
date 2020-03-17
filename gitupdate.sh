eddbc79f5c11a37c80ea76046683a4367b944e7a
radio-m <doc.zerg@gmail.com>
2020-03-17 09:50:38 +0200

    Westen fixes

---
+++ gitupdate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /config
+git add .
+git status
+echo -n "Enter the Description for the Change: " [Minor Update]
+read CHANGE_MSG
+git commit -m "${CHANGE_MSG}"
+git push origin master
+
+exit
